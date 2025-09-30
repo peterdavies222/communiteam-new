8a29a21501b1e900a5e16e898c97f2a2d72a5ed8
peterdavies222 <peterdavies222@gmail.com>
2025-09-30 18:33:40 +0800

    splash

--- src/views/partials/splash.js
+++ src/views/partials/splash.js
@@ -4,7 +4,7 @@ const splash = html`
 
   <div class="app-splash">
     <div class="inner">
-      <img class="app-logo" src="/images/ct-logo.svg" />
+      <img class="app-logo" src="/images/logo.svg" />
       <sl-spinner style="font-size: 2em;"></sl-spinner>
     </div>
   </div>
